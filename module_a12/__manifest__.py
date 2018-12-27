984b44766daeaca7565545cd555997255fa2cd87
Enric Tobella <etobella@creublanca.es>
2018-12-27 14:25:46 +0100

    FIX

---
+++ module_a12/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2017 Creu Blanca
+# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'Module',
+    'version': '11.0.1.0.0',
+    'author': "Odoo Community Association (OCA)",
+    'summary': 'Based on ISO 15489',
+    'category': 'Medical',
+    'depends': [
+        'module_a11',
+        'module_b11',
+        'module_c11',
+    ],
+    'website': 'https://github.com/OCA/vertical-medical',
+    "license": "LGPL-3",
+    "data": [
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'auto_install': False,
+}
---
+++ module_c11/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2017 Creu Blanca
+# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'Module',
+    'version': '11.0.1.0.0',
+    'author': "Odoo Community Association (OCA)",
+    'summary': 'Based on ISO 15489',
+    'category': 'Medical',
+    'depends': [
+        'module_a10',
+        'module_b10',
+        'module_c10',
+    ],
+    'website': 'https://github.com/OCA/vertical-medical',
+    "license": "LGPL-3",
+    "data": [
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'auto_install': False,
+}
